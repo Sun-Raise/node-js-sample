a28d7a8e9ea98842e888bd65286cb3755112f7d6
anandgit12 <42269935+anandgit12@users.noreply.github.com>
2018-11-16 08:51:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World , Welome to Anand's Page!')
+  response.send('Hello World to the zeep!')
 })
 
 app.listen(app.get('port'), function() {
